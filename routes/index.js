a5065923bdfc5707a58069046147302d2e011d7b
juan eugenio <leuviah77@gmail.com>
2021-10-14 22:12:50 +0200

    Styling details

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,17 @@ const isLoggedIn = require("../middleware/isLoggedIn");
 const User = require("../models/User.model");
 
 /* GET home page */
-router.get("/", isLoggedIn, (req, res) => {
-  Post.find({ user: req.session.user._id }).then((allPosts) => {
-    res.render("index", { allPosts });
-  });
+router.get("/", (req, res) => {
+	Post.find().then((allPosts) => {
+		res.render("index", { allPosts });
+	});
 });
+
+// Home Page with user validation.
+//  router.get("/", isLoggedIn, (req, res) => {
+//   Post.find({ user: req.session.user._id }).then((allPosts) => {
+//     res.render("index", { allPosts });
+//   });
+// });
 
 module.exports = router;
